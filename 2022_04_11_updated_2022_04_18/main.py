eecf2edf6d4fa6d60d19323d494444c176a59781
Student <student@itstep.local>
2022-04-18 20:01:10 +0300

    Показана реализация класса Stack

---
+++ 2022_04_11_updated_2022_04_18/main.py
@@ -0,0 +1,42 @@
+class Stack:
+
+    class EmptyError(Exception):
+        pass
+
+    class FullError(Exception):
+        pass
+
+    def __init__(self, max_size):
+        self.__max_size = int(max_size)
+        if self.__max_size < 1:
+            raise ValueError("Stack size can not be less than 1.")
+        self.__body = []
+
+    def push(self, item):
+        if len(self.__body) < self.__max_size:
+            self.__body.append(item)
+        else:
+            raise Stack.FullError("Stack is full.")
+
+    def pop(self):
+        try:
+            item = self.__body.pop()
+        except IndexError:
+            raise Stack.EmptyError("Stack is empty.")
+
+        return item
+
+
+if __name__ == '__main__':
+
+    # Создаём стек на 10 элементов
+    s = Stack(10)  # Стек пуст!
+
+    # Кладём в стек три объекта
+    s.push("3434")
+    s.push(3)
+    s.push([1, 2, 4])
+
+    # Вынимаем из стека объект и выводим его на экран
+    obj = s.pop()  # После этой операции в стеке остаётся только 2 объекта!
+    print(obj) # [1, 2, 4]
