cc71f935d808b75084a74cbb328b23427295d3b5
Student <student@itstep.local>
2022-04-25 22:10:39 +0300

    Включена поддержка внешних ключей

--- 2022_04_25/ex1.py
+++ 2022_04_25/ex1.py
@@ -8,7 +8,9 @@ DB_FILE_NAME = WORK_DIR / 'db.db'
 
 
 SQL_SCRIPT_CREATE_ALL = """
+PRAGMA foreign_keys=on;
 BEGIN TRANSACTION;
+-- Создаём таблицы --
 CREATE TABLE IF NOT EXISTS Author (
     author_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     first_name TEXT(100) NOT NULL,
@@ -21,13 +23,32 @@ CREATE TABLE IF NOT EXISTS Book (
     FOREIGN KEY(author_id) REFERENCES Author(author_id)
         -- При удалении автора, удаляются все его книги (по цепочке)
         ON DELETE CASCADE
+        -- При обновлении автора, обновляются все его упоминания
+        ON UPDATE CASCADE
 );
+CREATE TABLE IF NOT EXISTS Publisher (
+    publisher_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT(100)
+);
+CREATE TABLE IF NOT EXISTS BookPublisher (
+    book_id INTEGER NOT NULL,
+    publisher_id INTEGER NOT NULL,
+    FOREIGN KEY(book_id) REFERENCES Book(book_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY(publisher_id) REFERENCES Publisher(publisher_id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE    
+); 
+-- Наполняем таблицы данными --
 INSERT OR REPLACE
     INTO Author(author_id, first_name, last_name) VALUES
         (1, "Мрак", "Невт")
         , (2, "Сократ", "")
         , (3, "Волк", "Серый");
--- ...
+INSERT OR REPLACE
+    INTO Book(book_id, title, author_id) VALUES
+        (1, "Как ловит мух", 2);
 COMMIT;
 """
 
--- 2022_04_25/ex1_console.py
+++ 2022_04_25/ex1_console.py
@@ -1,3 +1,6 @@
+PAGE_BREAK = "\n"
+
+
 def print_authors(cursor):
     cursor.execute("""
         SELECT author_id, first_name, last_name
@@ -40,6 +43,26 @@ def add_book(cursor, title, author_id):
     cursor.connection.commit()
 
 
+def delete_author(cursor, author_id):
+    sql = """
+    DELETE
+        FROM Author
+            WHERE Author.author_id = ?; 
+    """
+    cursor.execute(sql, (author_id, ))
+    cursor.connection.commit()
+
+
+def delete_book(cursor, book_id):
+    sql = """
+    DELETE
+        FROM Book
+            WHERE Book.book_id = ?; 
+    """
+    cursor.execute(sql, (book_id, ))
+    cursor.connection.commit()
+
+
 def get_number(prompt, number_type, default=None):
     while True:
         try:
@@ -60,8 +83,15 @@ def start_menu_add_book(cursor):
     print("Добавление книги")
     title = input("Вв название книги: ")
     author_id = get_number("Вв ID автора: ", int)
-    print("\n\n")
+    print(PAGE_BREAK)
     add_book(cursor, title, author_id)
+
+
+def start_menu_delete_author(cursor):
+    print("Удаление автора")
+    author_id = get_number("Вв ID автора: ", int)
+    print(PAGE_BREAK)
+    delete_author(cursor, author_id)
 
 
 def start_menu(cursor):
@@ -71,18 +101,24 @@ def start_menu(cursor):
         print("2 - показать книги")
         print("3 - добавить автора")
         print("4 - добавить книгу")
+        print("5 - удалить автора")
+        print("6 - удалить книгу")
         n = get_number(">>> ", int)
-        print("\n\n")
+        print(PAGE_BREAK)
 
         if n == 0:
             break
         elif n == 1:
             print_authors(cursor)
-            print("\n\n")
+            print(PAGE_BREAK)
         elif n == 2:
             print_books(cursor)
-            print("\n\n")
+            print(PAGE_BREAK)
         elif n == 3:
             pass
         elif n == 4:
             start_menu_add_book(cursor)
+        elif n == 5:
+            start_menu_delete_author(cursor)
+        elif n == 6:
+            pass
